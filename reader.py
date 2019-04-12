8dc41d0ff20df448a47274abfb9668f345771f7f
Kanha Srivastav <kanha16155@iiitd.ac.in>
2019-04-12 21:16:10 +0530

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,20 +1,21 @@
 import cv2
 import numpy
+
 count = 0
-while (count < 100):
-	img = cv2.imread("pictures/frame"+str(count)+".jpg", flags=cv2.IMREAD_COLOR)	
-	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) 
-	lower_red = numpy.array([0,50,50]) 
-	upper_red = numpy.array([10,255,255]) 
-	mask = cv2.inRange(hsv, lower_red, upper_red)
-	res = cv2.bitwise_and(img,img, mask= mask) 
+while (count < 100): #100 images coming from cameraRUN.py
+	img = cv2.imread("pictures/frame"+str(count)+".jpg", flags=cv2.IMREAD_COLOR) #reads image from file
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #converts pixel values from RGB to HSV
+	lower_red = numpy.array([0,50,50]) #sets lower hue value to be read
+	upper_red = numpy.array([10,255,255]) #sets highest value to be red
+	mask = cv2.inRange(hsv, lower_red, upper_red) #creates a mask which shows the pixels that fall into the upper and lower bounds
+	res = cv2.bitwise_and(img,img, mask = mask) #and operation with image and mask created
 	cv2.imshow('frame',img) 
 	cv2.imshow('mask',mask) 
 	cv2.imshow('res',res)
-	k = cv2.waitKey(1) & 0xFF
-	key = cv2.waitKey(1) & 0xFF
-	if k == ord("q"): 
-		count +=1
-	#if key == ord("r"):
+	k = cv2.waitKey(1) & 0xFF 
+	#key = cv2.waitKey(1) & 0xFF
+	if k == ord("q"): #pressing 'q' increments the images shown
+		count += 1
+	#if key == ord("r"): #does not work well
 		#break
-cv2.destroyAllWindows() 
+cv2.destroyAllWindows() #closes all open windows after all images are read
